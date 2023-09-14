798e81fc021733a8e0b514248eb05cc3a47cf910
Kazuhiiro <64940750+Kazuhiiro@users.noreply.github.com>
2023-09-14 16:56:55 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,14 +1,18 @@
 import cors from 'cors'
 import express from 'express'
 
+import { download } from "./download.js"
+
 const app = express()
 app.use(cors)
 
 /*
 Receber as informações e entregar uma resposta
 */
-app.get('/summary', (request, response) => {
-    response.send('Hello, World')
+app.get('/summary/:id', (request, response) => {
+    download(request.params.id)
+
+    response.json({ result: "Download do vídeo realizado com sucesso!" })
 })
 
 app.listen(3333, () => console.log("Server is running on port 3333"))
